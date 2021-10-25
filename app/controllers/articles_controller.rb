363002eb2f9721501456db68b20221601f067394
chanel donahoe <cdonahoe05@gmail.com>
2021-10-25 23:49:12 +1100

    created new and create path, actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,27 @@
 class ArticlesController < ApplicationController
 
-def show 
-  @article = Article.find(params[:id])
-end 
+  def show 
+    @article = Article.find(params[:id])
+  end 
 
-def index 
-  @articles = Article.all
-end 
+  def index 
+    @articles = Article.all
+  end 
+
+  def new
+    @article = Article.new
+
+  end 
+
+  def create
+    #'white list' what we allow - basically filtering through
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was succesfully created."
+      redirect_to article_path(@article)
+    else 
+      render 'new'
+    end 
+  end 
 
 end 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  #There are many validations to do this 
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :description, presence: true, length: {minimum: 6, maximum: 255}
 end 
