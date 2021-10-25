2847a83cf5328d5ddb8d71e72d897a7ac1ab33a1
chanel donahoe <cdonahoe05@gmail.com>
2021-10-26 00:10:39 +1100

    created destroy route, view and added action in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,4 +24,25 @@ class ArticlesController < ApplicationController
     end 
   end 
 
+  def edit 
+    @article = Article.find(params[:id])
+
+  end 
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was udpated successfully"
+      redirect_to @article
+    else 
+      render 'edit'
+    end 
+  end 
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end 
+
 end 
