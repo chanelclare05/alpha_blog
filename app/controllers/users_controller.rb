4a6f6639fad09f4369a10124e848c2d87f75965c
chanel donahoe <cdonahoe05@gmail.com>
2021-10-27 22:35:05 +1100

    Created a show page for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new 
     @user = User.new
   end 
@@ -13,16 +15,32 @@ class UsersController < ApplicationController
     end 
   end 
 
+  def edit 
+  
+  end 
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else 
+      render 'new'
+    end 
+  end 
+
+  def show
+    @articles = @user.articles
+  end 
+
   private 
 
-  # def set_user
-  #   @user = Article.find(params[:id])
-  # end 
+  def set_user
+    @user = User.find(params[:id])
+  end 
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end 
-
 
 
 end
