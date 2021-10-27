bab2cfc3331d40fe27667b2db7680517f8005cad
chanel donahoe <cdonahoe05@gmail.com>
2021-10-27 22:05:34 +1100

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,26 @@ class UsersController < ApplicationController
     @user = User.new
   end 
 
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have succesfully signed up!"
+      redirect_to articles_path
+    else 
+      render 'new'
+    end 
+  end 
+
+  private 
+
+  # def set_user
+  #   @user = Article.find(params[:id])
+  # end 
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end 
+
+
 
 end
