5ae5f5ea4f270261a4fdc6961ace12774feb8e5c
chanel donahoe <cdonahoe05@gmail.com>
2021-10-28 17:47:18 +1100

    Added User profile navigation to nav bar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     #'white list' what we allow - basically filtering through
     @article = Article.new(article_params)
-    @article.user = User.last
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was succesfully created."
       redirect_to article_path(@article)
