aa236b6ea24f204e8ba4a7ad3cfec860c0bc8946
chanel donahoe <cdonahoe05@gmail.com>
2021-10-28 16:23:08 +1100

    added validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  #allows action to be available to the views aswell as the controllers
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end 
+
+  def logged_in?
+    #Turns the variable current_user into a boolean value
+    !!current_user 
+  end 
 
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     #'white list' what we allow - basically filtering through
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.last
     if @article.save
       flash[:notice] = "Article was succesfully created."
       redirect_to article_path(@article)
