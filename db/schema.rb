fc68f919d8cb4955cc8716e9d74b4e793101bc55
chanel donahoe <cdonahoe05@gmail.com>
2021-10-29 09:25:31 +1100

    Created admin field in Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_10_26_121650) do
-=======
-ActiveRecord::Schema.define(version: 2021_10_26_062311) do
->>>>>>> create-users-table-model
+ActiveRecord::Schema.define(version: 2021_10_28_075424) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -30,13 +26,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_062311) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
