2201252d45080b1961c706121a2191de08ae9f61
chanel donahoe <cdonahoe05@gmail.com>
2021-11-06 23:28:05 +1100

    Added many to many associations via the articlecategory model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   #There are many validations to do this 
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :description, presence: true, length: {minimum: 6, maximum: 255}
